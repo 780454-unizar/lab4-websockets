fabadb844f64e884eddd6deb90c7cfaed5c05a4d
780454-unizar <780454@unizar.es>
2021-11-23 18:05:53 +0100

    Primary goal

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -37,7 +37,6 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -46,8 +45,14 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        //COMPLETE ME
+        assertEquals(4, list.size)
+        val answers = listOf("Tell me more about such feelings.",
+                "Do you often feel happy?",
+                "Do you enjoy feeling happy?",
+                "Why do you feel that way?")
+        
+        assert(list[3] in answers)
     }
 
 }
