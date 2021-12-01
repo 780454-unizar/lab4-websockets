27e5701058999545a52e1f6e597bb48bf5626297
780454-unizar <79200103+780454-unizar@users.noreply.github.com>
2021-12-01 21:05:58 +0100

    Update ElizaServerTest.kt

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -37,7 +37,6 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
     @Test
     fun onChat() {
         val latch = CountDownLatch(4)
@@ -46,8 +45,10 @@ class ElizaServerTest {
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        val answers = listOf("Tell me more about such feelings.", "Do you often feel happy?",
+                            "Do you enjoy feeling happy?", "Why do you feel that way?")
+        assertEquals(4, list.size)
+        assertEquals(list[3] in answers)
     }
 
 }
@@ -68,8 +69,8 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session)  {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        if (latch.count == 1L) {
+            session.basicRemote.sendText("I feel happy")
+        }
     }
 }
